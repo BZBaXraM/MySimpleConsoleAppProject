9e7f2ec766e9a92730f6ac1e62e249879b424e42
Bahram <baxram1997007@gmail.com>
2024-01-20 19:57:56 +0400

    added star wars app with rest api

--- MiniCms.Mvc/Models/CustomerViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MiniCms.Mvc.Models;
-
-public class CustomerViewModel
-{
-    public string Name { get; set; } = string.Empty;
-    public string Surname { get; set; } = string.Empty;
-    public string Phone { get; set; } = string.Empty;
-    public string Notes { get; set; } = string.Empty;
-}
